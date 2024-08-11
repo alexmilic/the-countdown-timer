c34610f886217f3042ba96fbf878fee63fe8b3ca
alexmilic <alexmilic01@gmail.com>
2024-08-11 14:36:36 +0200

    Added new component and ref forwarding

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,16 @@
+import { forwardRef } from "react";
+
+const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
+  return ( 
+    <dialog ref={ref}>
+      <h2>You {result}</h2>
+      <p>The target time was <strong>{targetTime}</strong></p>
+      <p>You stopped the timer with <strong>X seconds left</strong></p>
+      <form action="">
+        <button>Close</button>
+      </form>
+    </dialog>
+  )
+});
+
+export default ResultModal;
